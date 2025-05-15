c2af826a97b11334c33ded1b818bb84010561983
Deva <manuelbocelli1@gmail.com>
2025-05-15 16:30:00 +0200

    movement
    aggiunto lo sprint (aggiornabile da inspector) e jump (non funziona sistemerò più tardi)

--- Assets/Scripts/Movimento/PlayerMovement.cs
+++ Assets/Scripts/Movimento/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -7,11 +8,30 @@ public class PlayerMovement : MonoBehaviour
     public Transform cam;
 
     public float speed = 6f;
-    
+
+    //float dello sprint
+    public float sprinting = 3f;
+
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
-    
+    //bool sprint
+    bool isSprinting = false;
+
+    //float jump
+    public float jumpSpeed;
+    private float ySpeed;
+    private bool isGrounded;
+
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+
+
+    //movement con smooth turn
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
@@ -26,6 +46,39 @@ public class PlayerMovement : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+       
+        //sprint + bool sopra
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isSprinting = !isSprinting; 
+            if (isSprinting)
+            {
+                speed += sprinting;
+            }
+            else
+            {
+                speed -= sprinting;
+            }
+        }
+
+        
+
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+        if (Input.GetButtonDown("jump"))
+        {
+            ySpeed = 0.5f;
+        }
+
+        if (controller.isGrounded)
+        {
+            ySpeed = 0.5f;
+                isGrounded = true;
+                if (Input.GetButtonDown("jump"))
+                {
+                 ySpeed = jumpSpeed;
+                 isGrounded = false;
+                }
         }
     }
 }
